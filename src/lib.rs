3184b1099871996985ce9bf716b5dec6302a397d
Eugene Chupriyanov <tchu@tchu.ru>
2018-10-10 13:26:03 +0300

    Fixed formatting

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,16 @@ mod tests {
     }
     #[test]
     fn echo_test() {
-      let input = "hello!".as_bytes();
-      let output: Vec<u8> = vec!(0; input.len());
-      unsafe {
-        let echo_out = echo(input.len() as u64, input.as_ptr() as *mut _, output.as_ptr()  as *mut _);
-        assert_eq!(echo_out, input.len() as u64);
-        assert_eq!(input.to_vec(), output);
-      }
+        let input = "hello!".as_bytes();
+        let output: Vec<u8> = vec![0; input.len()];
+        unsafe {
+            let echo_out = echo(
+                input.len() as u64,
+                input.as_ptr() as *mut _,
+                output.as_ptr() as *mut _,
+            );
+            assert_eq!(echo_out, input.len() as u64);
+            assert_eq!(input.to_vec(), output);
+        }
     }
 }
