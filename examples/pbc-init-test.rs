d53ec2a6270bfbe956a3ba905d7c72aaf0cde02b
David McClain <dbm@refined-audiometrics.com>
2018-10-10 18:52:07 -0700

    BLS Signatures, fake Hash

--- examples/pbc-init-test.rs
+++ examples/pbc-init-test.rs
@@ -1,8 +1,10 @@
 extern crate rust_libpbc;
-// extern crate rust_gmp;
+extern crate gmp;
 
 use std::fmt;
 use std::mem;
+
+// -------------------------------------------------------------------
 
 const PBC_CONTEXT_AR160 : u8 = 0;
 const NAME_AR160 : &str = "AR160";
@@ -14,9 +16,15 @@ exp2 159
 exp1 107
 sign1 1
 sign0 1";
+const ORDER_AR160 : &str = "730750818665451621361119245571504901405976559617";
 const G1_FR256 : &str = "ff8f256bbd48990e94d834fba52da377b4cab2d3e2a08b6828ba6631ad4d668500";
 const G2_FR256 : &str = "e20543135c81c67051dc263a2bc882b838da80b05f3e1d7efa420a51f5688995e0040a12a1737c80def47c1a16a2ecc811c226c17fb61f446f3da56c420f38cc01";
+const ZR_SIZE_FR256 : usize = 32;
+const G1_SIZE_FR256 : usize = 33;
+const G2_SIZE_FR256 : usize = 65;
+const GP_SIZE_FR256 : usize = 384;
 
+// -------------------------------------------------------------------
 
 const PBC_CONTEXT_FR256 : u8 = 1;
 const NAME_FR256 : &str = "FR256";
@@ -27,27 +35,27 @@ b 3
 beta 76600213043964638334639432839350561620586998450651561245322304548751832163977
 alpha0 82889197335545133675228720470117632986673257748779594473736828145653330099944
 alpha1 66367173116409392252217737940259038242793962715127129791931788032832987594232";
+const ORDER_FR256 : &str = "115792089237314936872688561244471742058035595988840268584488757999429535617037";
 const G1_AR160 : &str = "797EF95B4B2DED79B0F5E3320D4C38AE2617EB9CD8C0C390B9CCC6ED8CFF4CEA4025609A9093D4C3F58F37CE43C163EADED39E8200C939912B7F4B047CC9B69300";
 const G2_AR160 : &str = "A4913CAB767684B308E6F71D3994D65C2F1EB1BE4C9E96E276CD92E4D2B16A2877AA48A8A34CE5F1892CD548DE9106F3C5B0EBE7E13ACCB8C41CC0AE8D110A7F01";
+const ZR_SIZE_AR160 : usize = 20;
+const G1_SIZE_AR160 : usize = 65;
+const G2_SIZE_AR160 : usize = 65;
+const GP_SIZE_AR160 : usize = 128;
+
+// -------------------------------------------------------------------
 
 struct PBCInfo {
     context      : u8,
     name         : *const str,
     text         : *const str,
-    g1_size      : u16,
-    g2_size      : u16,
-    pairing_size : u16,
-    field_size   : u16,
+    g1_size      : usize,
+    g2_size      : usize,
+    pairing_size : usize,
+    field_size   : usize,
+    order        : *const str,
     g1           : *const str,
     g2           : *const str
-}
-
-fn bv_to_str(x : &[u8]) -> String {
-    let mut s = String::new();
-    for ix in 0 .. x.len() {
-        s.push_str(&format!("{:02x}", x[ix]));
-    }
-    s
 }
 
 const CURVES : &[PBCInfo] = &[
@@ -55,10 +63,11 @@ const CURVES : &[PBCInfo] = &[
         context      : PBC_CONTEXT_AR160,
         name         : NAME_AR160,
         text         : INIT_TEXT_AR160,
-        g1_size      :  65,
-        g2_size      :  65,
-        pairing_size : 128,
-        field_size   :  20,
+        g1_size      : G1_SIZE_AR160,
+        g2_size      : G2_SIZE_AR160,
+        pairing_size : GP_SIZE_AR160,
+        field_size   : ZR_SIZE_AR160,
+        order        : ORDER_AR160,
         g1           : G1_AR160,
         g2           : G2_AR160},
 
@@ -66,16 +75,18 @@ const CURVES : &[PBCInfo] = &[
         context      : PBC_CONTEXT_FR256,
         name         : NAME_FR256,
         text         : INIT_TEXT_FR256,
-        g1_size      :  33,
-        g2_size      :  65,
-        pairing_size : 384,
-        field_size   :  32,
+        g1_size      : G1_SIZE_FR256,
+        g2_size      : G2_SIZE_FR256,
+        pairing_size : GP_SIZE_FR256,
+        field_size   : ZR_SIZE_FR256,
+        order        : ORDER_FR256,
         g1           : G1_FR256,
         g2           : G2_FR256},
 ];        
 
+// -------------------------------------------------------------------
 // collect a vector of 8-bit values from a hex string.
-fn str_to_u8vec(s: &str, x: &mut [u8]) {
+fn str_to_u8v(s: &str, x: &mut [u8]) {
     let nx = x.len();
     let mut pos = 0;
     let mut val: u8 = 0;
@@ -108,19 +119,59 @@ fn str_to_u8vec(s: &str, x: &mut [u8]) {
     }
 }
 
+fn u8v_to_str(x : &[u8]) -> String {
+    let mut s = String::new();
+    for ix in 0 .. x.len() {
+        s.push_str(&format!("{:02x}", x[ix]));
+    }
+    s
+}
+
+/*
+struct ByteVec(Vec<u8>);
+struct HexString(String);
+
+impl ByteVec {
+    fn to_hexstr(self) -> String {
+        let mut s = String::new();
+        for v in self.0 {
+            s.push_str(&format!("{:02x}", v));
+        }
+        s
+    }
+
+    fn of_size(len : usize) -> ByteVec {
+        ByteVec(vec![0; len])
+    }
+
+    fn from_str(s : &str) -> ByteVec {
+        let mut bv : Vec<u8> = vec![0; s.len() >> 1];
+        str_to_u8v(&s, &mut bv);
+        ByteVec(bv)
+    }
+}
+
+impl HexString {
+    fn to_byteVec(self) -> ByteVec {
+        let len = self.0.len();
+        let mut bv : Vec<u8> = vec![0; len >> 1];
+        str_to_u8v(&self.0, &mut bv);
+        ByteVec(bv)
+    }
+}
+*/
+// -------------------------------------------------------------------
+
 fn init_pairings() {
-    let mut psize : [u64;4] = [0;4];
-    let mut context = 0;
-    let mut v : [u8; 65] = [0;65]; // sufficient for longest generator
-    let mut ans : i64 = 0;
+    let psize : [u64;4] = [0;4];
     for info in CURVES {
-        context = info.context as u64;
+        let context = info.context as u64;
         unsafe {
             println!("Init curve {}", (*info.name).to_string());
             println!("Context: {}", context);
             println!("{}", (*info.text).to_string());
 
-            ans = rust_libpbc::init_pairing(
+            let ans = rust_libpbc::init_pairing(
                 context,
                 info.text as *mut _,
                 (*info.text).len() as u64,
@@ -132,28 +183,45 @@ fn init_pairings() {
             assert_eq!(psize[2], info.pairing_size as u64);
             assert_eq!(psize[3], info.field_size as u64);
 
-            assert!(psize[0] <= v.len() as u64);
-            assert!(psize[1] <= v.len() as u64);
-
-            str_to_u8vec(&(*info.g1), &mut v);
-                println!("G1: {}", bv_to_str(&v[0..info.g1_size as usize]));
-            ans = rust_libpbc::set_g1(
+            let mut v1 : Vec<u8> = vec![0; info.g1_size];
+            str_to_u8v(&(*info.g1), &mut v1);
+            println!("G1: {}", u8v_to_str(&v1));
+            let len = rust_libpbc::set_g1(
                 context,
-                v.as_ptr() as *mut _);
+                v1.as_ptr() as *mut _);
             // returns nbr bytes read, should equal length of G1
-            assert_eq!(ans, psize[0] as i64);
+            assert_eq!(len, info.g1_size as i64);
 
-            str_to_u8vec(&(*info.g2), &mut v);
-                println!("G2: {}", bv_to_str(&v[0..info.g2_size as usize]));
-            ans = rust_libpbc::set_g2(
+            let mut v1 : Vec<u8> = vec![0; info.g1_size];
+            let len = rust_libpbc::get_g1(
                 context,
-                v.as_ptr() as *mut _);
+                v1.as_ptr() as *mut _,
+                info.g1_size as u64);
+            assert_eq!(len, info.g1_size as u64);
+            println!("G1 readback: {}", u8v_to_str(&v1));
+            
+            let mut v2 : Vec<u8> = vec![0; info.g2_size];
+            str_to_u8v(&(*info.g2), &mut v2);
+            println!("G2: {}", u8v_to_str(&v2));
+            let len = rust_libpbc::set_g2(
+                context,
+                v2.as_ptr() as *mut _);
             // returns nbr bytes read, should equal length of G2
-            assert_eq!(ans, psize[1] as i64);
+            assert_eq!(len, info.g2_size as i64);
+
+            let mut v2 : Vec<u8> = vec![0; info.g2_size];
+            let len = rust_libpbc::get_g2(
+                context,
+                v2.as_ptr() as *mut _,
+                info.g2_size as u64);
+            assert_eq!(len, info.g2_size as u64);
+            println!("G2 readback: {}", u8v_to_str(&v2));
+            
         }
         println!("");
     }
 }
+// ------------------------------------------------------------------------
 
 fn main() {
     init_pairings();
@@ -174,10 +242,68 @@ fn main() {
 }
 
 // -----------------------------------------------------
-/*
-struct PBC(u8);
 
-impl PBC {
-    fn 
+const HASH_SIZE : usize = 32;
+
+#[derive(Copy, Clone)]
+struct Zr([u8;ZR_SIZE_FR256]);
+
+#[derive(Copy, Clone)]
+struct G1([u8;G1_SIZE_FR256]);
+
+#[derive(Copy, Clone)]
+struct G2([u8;G2_SIZE_FR256]);
+
+#[derive(Copy, Clone)]
+struct Hash([u8;HASH_SIZE]);
+
+#[derive(Copy, Clone)]
+struct PbcSecretKey (Zr);
+
+#[derive(Copy, Clone)]
+struct PbcPublicKey (G2);
+
+struct PbcBlsSignature {
+    sig  : G1,
+    pkey : PbcPublicKey
 }
-*/
+
+fn hash(_msg : &[u8]) -> Hash {
+    // dummy stub for now
+    Hash([0;HASH_SIZE])
+}
+
+fn sign_hash(h : &Hash, skey : &PbcSecretKey) -> G1 {
+    let mut v : [u8; G1_SIZE_FR256] = [0; G1_SIZE_FR256];
+    unsafe {
+        rust_libpbc::sign_hash(
+            PBC_CONTEXT_FR256 as u64,
+            v.as_ptr() as *mut _,
+            (skey.0).0.as_ptr() as *mut _,
+            h.0.as_ptr() as *mut _,
+            HASH_SIZE as u64);
+    }
+    G1(v)
+}
+
+fn check_hash(h : &Hash, sig : &G1, pkey : &PbcPublicKey) -> bool {
+    unsafe {
+        0 == rust_libpbc::check_signature(
+                PBC_CONTEXT_FR256 as u64,
+                sig.0.as_ptr() as *mut _,
+                h.0.as_ptr() as *mut _,
+                HASH_SIZE as u64,
+                (pkey.0).0.as_ptr() as *mut _)
+    }
+}
+
+fn sign_message(msg : &[u8], skey : &PbcSecretKey, pkey : &PbcPublicKey) -> PbcBlsSignature {
+    PbcBlsSignature {
+        sig  : sign_hash(&hash(&msg), skey),
+        pkey : pkey.clone()
+    }
+}
+
+fn check_message(msg : &[u8], sig : &PbcBlsSignature) -> bool {
+    check_hash(&hash(&msg), &sig.sig, &sig.pkey)
+}
