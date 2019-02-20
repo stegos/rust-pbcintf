9ffd7ba46d4befa669a7d73bef342945137b26dc
Eugene Chupriyanov <tchu@tchu.ru>
2019-02-20 14:54:17 +0300

    Fix linking issue under linux

--- build.rs
+++ build.rs
@@ -34,6 +34,7 @@ fn main() {
         .expect("Couldn't write bindings!");
 
     println!("cargo:rustc-link-search=/usr/local/lib");
+    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
     println!("cargo:rustc-link-lib=static=pbc");
     println!("cargo:rustc-link-lib=static=gmp");
 }
